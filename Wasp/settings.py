60420fee60f4068d3d1b81a6096dccbd5f4e5c57
IamRodion <22568039+IamRodion@users.noreply.github.com>
2024-03-07 00:44:40 -0500

    Delete config.py Borra el archivo config.py que almacenaba las secret keys aparte, y modifica el archivo gitignore

--- Wasp/settings.py
+++ Wasp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-from .config import KEY
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-^@(epqhaxj6cec$6$l!fz5!6eya@scp$&=6b8=r$*3jruc_k+8'
-SECRET_KEY = KEY
+SECRET_KEY = 'django-insecure-^@(epqhaxj6cec$6$l!fz5!6eya@scp$&=6b8=r$*3jruc_k+8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
